3225e2a4568c63c15a0f3808b4f2e65d026c2429
Anna Tolstova <anna_tolstova@hotmail.com>
2023-12-24 01:09:36 +0200

    fix: add cors headers

--- src/cart-service-stack.ts
+++ src/cart-service-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AuthorizationType, LambdaIntegration, LambdaRestApi, ResponseType, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from 'constructs';
 
@@ -8,17 +9,31 @@ require('dotenv').config();
 export class CartServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+  
     const cartApiLambda = new lambda.Function(this, "CartApiLambda", {
       code: lambda.Code.fromAsset("dist"),
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "main.handler"
+      handler: "main.handler",
     });
 
-    const api = new RestApi(this, "Api");
-    const cartResource = api.root.addResource("cart");
-    cartResource.addProxy({
-      defaultIntegration: new LambdaIntegration(cartApiLambda, { proxy: true }),
+    const api = new LambdaRestApi(this, "CartApi", {
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["*"],
+        allowOrigins: ["*"],
+        allowMethods: ["*"],
+        allowCredentials: true
+      },
+      handler: cartApiLambda
+    });
+
+    api.addGatewayResponse("GatewayResponse4XX", {
+      type: ResponseType.DEFAULT_4XX,
+      responseHeaders: {
+        "Access-Control-Allow-Origin": "'*'",
+        "Access-Control-Allow-Headers":
+          "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "Access-Control-Allow-Methods": "'OPTIONS,GET,PUT'"
+      },
     });
 
     new cdk.CfnOutput(this, "cartApiUrl", {value: api.url});
